00015f6ff06f748c2497a357a26e54f69f787f5b
biddabari <biddabari605@gmail.com>
2026-01-11 13:49:33 +0000

    Stop Generation Command

--- src/pages.config.js
+++ src/pages.config.js
@@ -51,11 +51,9 @@ import Permissions from './pages/Permissions';
 import Procurement from './pages/Procurement';
 import ProdhanComIntegration from './pages/ProdhanComIntegration';
 import ProductAnalytics from './pages/ProductAnalytics';
-import PurchaseOrders from './pages/PurchaseOrders';
 import ReportBuilder from './pages/ReportBuilder';
 import ReportGenerator from './pages/ReportGenerator';
 import Reports from './pages/Reports';
-import Sales from './pages/Sales';
 import ScheduledReports from './pages/ScheduledReports';
 import SendEmail from './pages/SendEmail';
 import Settings from './pages/Settings';
@@ -66,13 +64,15 @@ import SubmittedReports from './pages/SubmittedReports';
 import SystemLogs from './pages/SystemLogs';
 import SystemOptimization from './pages/SystemOptimization';
 import ThirdPartyApps from './pages/ThirdPartyApps';
-import UserAccessManager from './pages/UserAccessManager';
 import Webhooks from './pages/Webhooks';
 import WhatsApp from './pages/WhatsApp';
 import WhatsAppWebhook from './pages/WhatsAppWebhook';
 import employees from './pages/employees';
 import expenses from './pages/expenses';
 import performanceHub from './pages/performance-hub';
+import PurchaseOrders from './pages/PurchaseOrders';
+import Sales from './pages/Sales';
+import UserAccessManager from './pages/UserAccessManager';
 import __Layout from './Layout.jsx';
 
 
@@ -130,11 +130,9 @@ export const PAGES = {
     "Procurement": Procurement,
     "ProdhanComIntegration": ProdhanComIntegration,
     "ProductAnalytics": ProductAnalytics,
-    "PurchaseOrders": PurchaseOrders,
     "ReportBuilder": ReportBuilder,
     "ReportGenerator": ReportGenerator,
     "Reports": Reports,
-    "Sales": Sales,
     "ScheduledReports": ScheduledReports,
     "SendEmail": SendEmail,
     "Settings": Settings,
@@ -145,13 +143,15 @@ export const PAGES = {
     "SystemLogs": SystemLogs,
     "SystemOptimization": SystemOptimization,
     "ThirdPartyApps": ThirdPartyApps,
-    "UserAccessManager": UserAccessManager,
     "Webhooks": Webhooks,
     "WhatsApp": WhatsApp,
     "WhatsAppWebhook": WhatsAppWebhook,
     "employees": employees,
     "expenses": expenses,
     "performance-hub": performanceHub,
+    "PurchaseOrders": PurchaseOrders,
+    "Sales": Sales,
+    "UserAccessManager": UserAccessManager,
 }
 
 export const pagesConfig = {
